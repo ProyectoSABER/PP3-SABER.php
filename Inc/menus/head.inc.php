2d9b90418f01095cb5bca982e0894877e2bbbd95
Adrian Finochio <proyecto.s.a.b.e.n@gmail.com>
2022-10-17 22:21:09 -0300

    revision de head

--- Inc/menus/head.inc.php
+++ Inc/menus/head.inc.php
@@ -31,10 +31,8 @@
   <!-- Font-icon css-->
   <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 <!-- DataTable -->
-<link rel="stylesheet" type="text/css" href="assets/plugins/DataTables/datatables.min.css"/>
- <script src="assets/plugins/DataTables/jquery.dataTables.js"></script>
- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+<link rel="stylesheet" type="text/css" href="../../assets/plugins/DataTables/datatables.min.css"/>
+ 
 
 
 </head>
