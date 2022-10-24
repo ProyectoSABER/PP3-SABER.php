16e17c3e946aa0629b86070a32a2ee84d009f670
Adrian Finochio <proyecto.s.a.b.e.n@gmail.com>
2022-10-23 23:17:13 -0300

    Se modifico nombre de popper.js

--- Inc/js/js.inc.php
+++ Inc/js/js.inc.php
@@ -1,5 +1,5 @@
 <script src="js/jquery-3.6.1.min.js"></script>
-<script src="js/popper.v2.11.6.min.js"></script>
+<script src="js/popper.min.js"></script>
 <!-- <script src="https://unpkg.com/@popperjs/core@2"></script> -->
 
 
@@ -8,8 +8,10 @@
 <!-- Bootstrap Core JavaScript -->
 <script src="js/main.js"></script>
 <script src="js/bootstrap.min.js"></script>
+<?php if(!empty($_SESSION['USUARIO_DNI'])){?>
 <script src="assets/plugins/DataTables/jquery.dataTables.js"></script>
 <script src="assets/plugins/DataTables/jquery.dataTables.min.js"></script>
+<?php } ?>
 <!-- FontAwesome -->
 <script src="js/fontawesonKit.js"></script>
 
@@ -17,9 +19,7 @@
 <!-- Toastr -->
 <script src="assets/plugins/toastr/toastr.min.js"></script>
 <!-- The javascript plugin to display page loading on top-->
-<?php if(!empty($_SESSION['USUARIO_DNI'])){?>
-<script src="js/plugins/pace.min.js"></script>
-<?php } ?>
+
 
 <!-- Page specific javascripts-->
 
--- login.php
+++ login.php
@@ -90,12 +90,9 @@ require_once './Handler/HandlerLogin.php';
   
 
   </section>
+  <?php require_once('./Inc/js/js.inc.php'); ?>
 
-  <!-- Essential javascripts for application to work-->
-  <script src="js/jquery-3.3.1.min.js"></script>
-  <script src="js/popper.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/main.js"></script>
+  
   <!-- The javascript plugin to display page loading on top-->
   <script src="js/plugins/pace.min.js"></script>
   <script src="js/Personalizados/ValidatorNewUser.js"></script>
