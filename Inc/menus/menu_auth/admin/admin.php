9e6aa409c367b128ab72d884b79a04420aa41e80
Ruggeri <martin.ruggeri@live.com>
2023-01-19 01:16:54 -0300

    Se agrega modal para carga de nuevo usuario, se agrega boton de modificacion de usuarios.

--- Inc/menus/menu_auth/admin/admin.php
+++ Inc/menus/menu_auth/admin/admin.php
@@ -36,16 +36,6 @@
     </div>
 
 
-  
-
-    <div class="row" style="color: green;" >
-      <div class="col-md-3" >
-        <div class="widget-small " style="color: green;" ><i class="icon fa fa-users fa-3x "style="color: green;" ></i>
-          <div class="info" style="color: green;" >
-          <button type="button" class="btn btn-primary"><h4>Nuevo Usuario</h4></button>
-          </div>
-        </div>
-      </div>
       <!-- <div class="col-md-3">
         <div class="widget-small info coloured-icon"><i class="icon fa fa-thumbs-o-up fa-3x"></i>
           <div class="info">
@@ -72,7 +62,73 @@
       </div> -->
     </div>
    
+<!--comienza modal-->
 
+<!-- Button trigger modal -->
+
+<div class="row" style="color: green;" >
+      <div class="col-md-3" >
+        <div class="widget-small " style="color: green;" ><i class="icon fa fa-users fa-3x "style="color: green;" ></i>
+          <div class="info" style="color: green;" >
+          <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newUser"><h4>Nuevo Usuario</h4></button>
+          <!-- Button trigger modal -->
+          </div>
+        </div>
+      </div>
+
+<!-- Modal -->
+<div class="modal fade" id="newUser" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Cargar Nuevo Usuario</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <!-- DATOS DEL USUARIO -->
+
+
+        <div class="form-floating mb-3">
+  <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+  <label for="floatingInput">Nombre</label>
+</div>
+<div class="form-floating mb-3">
+  <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+  <label for="floatingInput">Apellido</label>
+</div>
+<div class="form-floating mb-3">
+  <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+  <label for="floatingInput">Documento</label>
+</div>
+<div class="form-floating mb-3" >
+  <input type="email" class="form-control" id="floatingInputValue" placeholder="name@example.com" value="mail@ejemplo.com">
+  <label for="floatingInputValue">Email</label>
+</div>
+<div class="form-floating">
+  <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+  <label for="floatingPassword">Password</label>
+</div>
+<br>
+<div class="form-floating">
+  <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
+    <option selected>Seleccione una opción</option>
+    <option value="1">1-Administrador</option>
+    <option value="2">2-Bibliotecario</option>
+    <option value="3">3-Docente</option>
+    <option value="3">4-Socio</option>
+    <option value="3">5-Visitante</option>
+  </select>
+  <label for="floatingSelect">Tipo de Usuario</label>
+</div>
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Guardar</button>
+      </div>
+    </div>
+  </div>
+</div>
 
 <div class="col-md-12">
   <div class="tile">
@@ -119,6 +175,7 @@
                 
                 <li><a class="dropdown-item" href="#">Eliminar <i class="fa fa-trash-o fa-fw"></i></a></li>
                 <li><a class="dropdown-item" href="#">Limitar Acceso <i class="fa fa-ban fa-fw"></i></a></li>
+                <li><a class="dropdown-item" href="#">Modificar<i class="fa fa-pencil fa-fw"></i></a></li>
                 
               </ul>
               </div>
